5329fad5f7bca126cd87898a7acf5bc1d2a53008
CristiFati <fati_utcluj@yahoo.com>
2023-02-02 12:20:02 +0200

    Minor: align

--- mue_idle.py
+++ mue_idle.py
@@ -53,7 +53,7 @@ def main(*argv):
 
 if __name__ == "__main__":
     print("Python {:s} {:03d}bit on {:s}\n".format(" ".join(item.strip() for item in sys.version.split("\n")),
-                                                    64 if sys.maxsize > 0x100000000 else 32, sys.platform))
+                                                   64 if sys.maxsize > 0x100000000 else 32, sys.platform))
     rc = main(*sys.argv[1:])
     print("\nDone.\n")
     sys.exit(rc)
