851fdb22644dc8804e4657e2dfd40770dc2be247
CristiFati <fati_utcluj@yahoo.com>
2023-02-03 22:49:30 +0200

    Bump version to 0.2

--- mue_idle.py
+++ mue_idle.py
@@ -16,7 +16,7 @@ from cfpyutils.common import ts_str
 from cfpyutils.keyboard import read_key
 
 
-__version_info__ = (0, 1)
+__version_info__ = (0, 2)
 __version__ = ".".join(str(e) for e in __version_info__)
 
 
